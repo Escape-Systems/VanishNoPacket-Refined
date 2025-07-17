a8597f4f85387998abc6d92ee7feac1dff1cf337
Lexie Malina <alex@t-ch.net>
2025-07-17 17:58:01 -0500

    Change to gradle for CI builds.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+import xyz.jpenilla.resourcefactory.paper.PaperPluginYaml
+
 group = "org.kitteh"
-version = "3.22"
+version = "3.22-SNAPSHOT"
 description = "VanishNoPacket-Refined"
 
 
@@ -8,6 +10,8 @@ plugins {
     `maven-publish`
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
     id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("xyz.jpenilla.resource-factory-paper-convention") version "1.3.0"
+    id("com.gradleup.shadow") version "9.0.0-rc1"
 }
 
 repositories {
@@ -22,7 +26,7 @@ repositories {
 
 dependencies {
 
-//    testImplementation(libs.junit.junit)
+    testImplementation("junit:junit:4.13.2")
     paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
     implementation("net.essentialsx:EssentialsX:2.21.1") {
         exclude("org.spigotmc")
@@ -44,10 +48,32 @@ publishing {
     }
 }
 
+tasks.shadowJar {
+    minimize()
+    archiveFileName.set("${project.name}-${project.version}.jar")
+}
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
 tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
+}
+
+paperPluginYaml {
+    main = "org.kitteh.vanish.VanishPlugin"
+    authors.add("Matt \"MBax\" Baxter")
+    authors.add("Lexie \"Tech\" Malina")
+    apiVersion = "1.21.4"
+    version = project.version.toString()
+    description = "Vanish for the high speed admin"
+
+    dependencies {
+        server("Essentials", PaperPluginYaml.Load.OMIT, false)
+        server("PlaceholderAPI", PaperPluginYaml.Load.OMIT, false)
+        server("Vault", PaperPluginYaml.Load.OMIT, false)
+        server("squaremap", PaperPluginYaml.Load.OMIT, false)
+        server("LuckPerms", PaperPluginYaml.Load.OMIT, false)
+    }
 }
