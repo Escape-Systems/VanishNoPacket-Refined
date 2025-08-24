2be2c7e5291f336d7d8ff93d708e19044075b52c
Lexie Malina <alex@t-ch.net>
2025-08-23 22:33:11 -0500

    Let's try this publishing thing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import io.papermc.hangarpublishplugin.model.Platforms
 import xyz.jpenilla.resourcefactory.paper.PaperPluginYaml
 
 group = "org.kitteh"
@@ -13,6 +14,7 @@ plugins {
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("xyz.jpenilla.resource-factory-paper-convention") version "1.3.0"
     id("com.gradleup.shadow") version "9.0.0-rc1"
+    id("io.papermc.hangar-publish-plugin") version "0.1.2"
 }
 
 repositories {
@@ -45,7 +47,8 @@ dependencies {
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_21
-paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
+paperweight.reobfArtifactConfiguration =
+    io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 publishing {
     publications.create<MavenPublication>("maven") {
@@ -64,6 +67,40 @@ tasks.withType<JavaCompile> {
 
 tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
+}
+
+hangarPublish {
+    publications.register("plugin") {
+        version = project.version.toString()
+        channel.set("Snapshot")
+        id.set("VanishNoPacket-Refined")
+        apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+
+        platforms {
+            register(Platforms.PAPER) {
+                jar.set(tasks.shadowJar.flatMap { it.archiveFile })
+                val versions: List<String> =
+                    (property("paperVersion") as String).split(",").map { it.trim() }
+                platformVersions.set(versions)
+
+                dependencies {
+                    hangar("Essentials") {
+                        required.set(false)
+                    }
+                    hangar("PlaceholderAPI") {
+                        required.set(false)
+                    }
+                    url("Vault", "https://www.spigotmc.org/resources/vault.34315/") {
+                        required.set(false)
+                    }
+                    hangar("squaremap") {
+                        required.set(false)
+                    }
+                    url("LuckPerms", "https://luckperms.net/download")
+                }
+            }
+        }
+    }
 }
 
 paperPluginYaml {
